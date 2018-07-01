5dbf5f79c8d75d34c3f1c0d74b5d915523d65769
Jack Pien <jack@jackpien.com>
2018-06-30 22:04:26 -0700

    .

--- devel_env/bash_profile_jp.bash
+++ devel_env/bash_profile_jp.bash
@@ -1,5 +1,14 @@
 export PS1=${debian_chroot:+($debian_chroot)}'\[\033[01;31m\]\u@\h\[\033[00m\]:\w\n\$ '
 
+# If this is an xterm set the title to user@host:dir
+case "$TERM" in
+xterm*|rxvt*)
+    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+    ;;
+*)
+    ;;
+esac
+
 function e() {
     # emacs -g 80x80 ${1} &
     emacs ${1} &
